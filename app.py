ccb7a058802f08fabc1cf5cfc39cb6e7c1779589
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-12-04 18:16:53 -0800

    finals week no sleep

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
-from flask import Flask
+import feedparser
+import requests
+from dateutil import parser
+from flask import Flask, render_template, redirect
+
+from forms import PodcastNewForm
+
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
@@ -7,5 +13,28 @@ app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/podcastnew', methods=['GET', 'POST'])
+def podcastnew():
+    form = PodcastNewForm()
+    if form.validate_on_submit():
+        url = form.podcast_url.data
+        site = requests.get(url)
+        with open('sitemap.xml', "w") as f:
+            f.write(site.text)
+        feed = feedparser.parse(url)
+        podcast_title = feed.channel.title
+        podcast_image = feed.channel.image['href']
+        podcast_summary = feed.channel.summary
+        podcast_author = feed.channel.author
+
+        for item in feed.entries:
+            title = item.title
+            link = item.link
+            url = item['links'][1]['href']
+            description = item.description
+            pub_date=parser.parse(item.published)
+        print("i", podcast_image, "t:", podcast_title, "l:", link, "u:", url, podcast_summary, "a:", podcast_author, description)
+        return render_template('podcast_new.html', form=form, feed=feed)
+    return render_template('podcast_submit.html', form=form)

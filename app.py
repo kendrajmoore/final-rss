9dc323c423347f725ce8288cb4001ccce480ed8f
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-12-08 09:40:05 -0800

    update

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+import re
+import feedparser
+from flask_wtf.form import FlaskForm
+from flask_moment import Moment
+from listennotes import podcast_api
+import requests
+from dateutil import parser
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask import Flask, render_template, redirect
+
+from forms import PodcastNewForm, SearchForm, LoginForm
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+def clean_text(text):
+    wordcount = {}
+    text = text.lower()
+    cleaned_text = re.sub(r'[^\w\s]','',text)
+    words = cleaned_text.split()
+    
+    
+    stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+    print(cleaned_text)
+               
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('login.html', form=form)
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        client = podcast_api.Client()
+        searched_podcast = form.input_podcast.data
+        response = client.search(q=searched_podcast)
+        res_json = response.json()
+        final_result = res_json['results']
+        return render_template('results.html', form=form, final_result=final_result)   
+    return render_template('search.html', form=form)
+
+@app.route('/podcastnew', methods=['GET', 'POST'])
+def podcastnew():
+    form = PodcastNewForm()
+    if form.validate_on_submit():
+        url = form.podcast_url.data
+        site = requests.get(url)
+        with open('sitemap.xml', "w") as f:
+            f.write(site.text)
+        feed = feedparser.parse(url)
+        print(feed)
+        return render_template('podcast_new.html', form=form, feed=feed)
+    return render_template('podcast_submit.html', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", title="Error"), 404
+
+@app.errorhandler(500)
+def server_error(error):
+    # note that we set the 404 status explicitly
+    return render_template('error.html', title="Error"), 500
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class PodcastNewForm(FlaskForm):
@@ -9,3 +9,8 @@ class PodcastNewForm(FlaskForm):
 class SearchForm(FlaskForm):
     input_podcast = StringField('Enter Podcast Name: ')
     submit = SubmitField('Submit')
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
